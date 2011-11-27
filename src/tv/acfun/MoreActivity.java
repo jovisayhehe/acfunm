150621f86eafb39826ea462f9ddb1604a2bc3a23
jovi <jovisayhehe@gmail.com>
2011-11-28 01:45:00 +0800

    ++

---
+++ src/tv/acfun/MoreActivity.java
@@ -0,0 +1,15 @@
+package tv.acfun;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MoreActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.more_layout);
+	}
+
+}
