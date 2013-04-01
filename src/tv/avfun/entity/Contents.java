4d18e93d2503ba18a46d7d0eb59e68917dd840ae
Yrom <yrom@qq.com>
2013-04-01 21:02:33 +0800

    新增Contents bean
    Just a todo

---
+++ src/tv/avfun/entity/Contents.java
@@ -0,0 +1,14 @@
+package tv.avfun.entity;
+
+//TODO 
+public class Contents {
+    private String title;
+    private String username;
+    private String description;
+    private long views; //TODO 或用int?
+    private String titleImg; // TODO 或用URL?
+    private int aid;
+    private int channelId;
+    private int comments;
+    
+}
