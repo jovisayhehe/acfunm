7f8c3490a142379a47fe3b1a9d9f461c3796bc22
Yrom Wang <zhuruoyun[at]gmail.com>
2013-07-22 17:15:06 +0800

    1. 加入楼层个数设置项
    2. 复用convertView，勉强加速一些些

--- src/tv/avfun/CommentsAdaper2.java
+++ src/tv/avfun/CommentsAdaper2.java
@@ -10,16 +10,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import tv.ac.fun.R;
+import tv.avfun.app.AcApp;
 import tv.avfun.entity.Comment;
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.text.Html;
 import android.text.Html.ImageGetter;
 import android.text.util.Linkify;
-import android.text.SpannedString;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,11 +33,12 @@ public class CommentsAdaper2 extends BaseAdapter {
     private TreeMap<Integer ,Comment> data;
     private List<Comment> comments = new ArrayList<Comment>();
     private Context mContext;
-
+    private int numOfFloor;
     public CommentsAdaper2(Context context, TreeMap<Integer ,Comment>data) {
         this.mInflater = LayoutInflater.from(context);
         this.mContext = context;
         this.data = data;
+        this.numOfFloor = AcApp.getConfig().getInt("num_of_floor", Build.VERSION.SDK_INT-3);
         for(Map.Entry<Integer ,Comment> e : data.entrySet()){
             comments.add(e.getValue());
         }
@@ -69,47 +69,53 @@ public class CommentsAdaper2 extends BaseAdapter {
         return position;
     }
 
-    private int frameId = R.id.comment_frame;
+    private int frameId = R.id.list_relative;
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        if(convertView != null && convertView instanceof ViewGroup){
-            ((ViewGroup)convertView).removeAllViews();
-            convertView = null;
-        }
-        // TODO 多个评论共同引用同一条的情况
-        Comment c = getItem(position);
-        //Log.i("comments", "--------position="+position);
-        RelativeLayout frame = (RelativeLayout) mInflater.inflate(R.layout.comments_listitem, null);
-        TextView username = (TextView) frame.findViewById(R.id.user_name);
-        username.setText("#"+c.count+" "+c.userName);
-        TextView content = (TextView) frame.findViewById(R.id.comments_content);
-        setContent(content,c);
+    	Comment c = getItem(position);
+        
+    	CommentViewHolder holder = null;
+    	if(convertView == null){
+    		holder = new CommentViewHolder();
+    		convertView= mInflater.inflate(R.layout.comments_listitem, null);
+    		holder.user  = (TextView) convertView.findViewById(R.id.user_name);
+    		holder.content = (TextView) convertView.findViewById(R.id.comments_content);
+    		convertView.setTag(holder);
+    	}else{
+    		holder = (CommentViewHolder) convertView.getTag();
+    		if(holder.hasQuote){
+    			((ViewGroup)convertView).removeView(holder.quoteFrame);
+    		}
+    		convertView.findViewById(R.id.requote).setVisibility(View.GONE);
+    	}
+    	holder.hasQuote = c.quoteId>0;
+    	holder.user.setText("#"+c.count+" "+c.userName);
+    	setContent(holder.content,c);
+        
         int quoteId = c.quoteId;
         Comment quote = data.get(quoteId);
         int anchor;
         if (quote != null) {
             if(quote.isQuoted){
                 if(quote.beQuotedPosition == position){
-                    handleQuote(quote,frame,10,position);
+                    handleQuote(quote,(RelativeLayout) convertView,numOfFloor,position);
                     anchor = frameId;
                 }else{
-                    frame.findViewById(R.id.requote).setVisibility(View.VISIBLE);
+                	convertView.findViewById(R.id.requote).setVisibility(View.VISIBLE);
                     anchor = R.id.requote;
                 }
             }else{
                 quote.isQuoted = true;
                 quote.beQuotedPosition = position;
-                handleQuote(quote,frame,10,position);
+                handleQuote(quote,(RelativeLayout) convertView,numOfFloor,position);
                 anchor = frameId;
             }
-            if(anchor!= R.id.requote){
-                RelativeLayout.LayoutParams userLayoutParams = (LayoutParams) username.getLayoutParams();
-                userLayoutParams.addRule(RelativeLayout.BELOW, anchor);
-                username.setLayoutParams(userLayoutParams);
-            }
+            RelativeLayout.LayoutParams userLayoutParams = (LayoutParams) holder.user.getLayoutParams();
+            userLayoutParams.addRule(RelativeLayout.BELOW, anchor);
+            holder.user.setLayoutParams(userLayoutParams);
         }
-        return frame;
+        return convertView;
     }
 
     private void setContent(TextView comment, Comment c) {
@@ -160,11 +166,16 @@ public class CommentsAdaper2 extends BaseAdapter {
         
         RelativeLayout quoteFrame = (RelativeLayout) mInflater.inflate(R.layout.comments_listitem, null);
         quoteFrame.setBackgroundResource(R.drawable.comment_bg);
+        quoteFrame.setId(frameId);
         RelativeLayout.LayoutParams params2 = new LayoutParams(-1, -2);
 //        params2.addRule(RelativeLayout.ABOVE, R.id.user_name);
         TextView username  = (TextView) quoteFrame.findViewById(R.id.user_name);
         username.setText("#"+quote.count+" "+quote.userName);
         commentFrame.addView(quoteFrame, params2);
+        if(commentFrame.getId() != frameId){
+	        CommentViewHolder holder = (CommentViewHolder) commentFrame.getTag();
+	        holder.quoteFrame = quoteFrame;
+        }
         TextView content = (TextView) quoteFrame.findViewById(R.id.comments_content);
         setContent(content,quote);
         int quoteId = quote.quoteId;
@@ -185,5 +196,11 @@ public class CommentsAdaper2 extends BaseAdapter {
             username.setLayoutParams(userLayoutParams);
         }
     }
-
+    static class CommentViewHolder{
+    	TextView user;
+    	TextView content;
+    	boolean hasQuote;
+    	RelativeLayout quoteFrame;
+    	
+    }
 }
