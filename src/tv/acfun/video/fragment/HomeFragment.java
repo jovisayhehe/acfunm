4320f0b80f4a3e1b2a0f056d89b8623649ab148e
yrom <yrom@qq.com>
2014-01-24 22:59:33 +0800

    fix position < 0

--- src/tv/acfun/video/fragment/HomeFragment.java
+++ src/tv/acfun/video/fragment/HomeFragment.java
@@ -124,6 +124,7 @@ public class HomeFragment extends RefreshActionGridFragment {
 
     @Override
     public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+        if(arg2 < 0 ) return;
         Video o = ((HomeAdapter) mAdapter).getVideoItem(arg2);
         boolean isArticleChannel = ((HomeActivity)mActivity).isArticleChannel(o.channelId);
         if(isArticleChannel){
